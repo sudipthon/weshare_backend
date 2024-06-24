ad1cf3856f5a855451ccab54db0cb851de4402a2
sudip <sudip00kc@gmail.com>
2024-06-24 23:56:41 +0545

    list of conversation along with participant detail in websocket working

--- Backend/Message/consumers.py
+++ Backend/Message/consumers.py
@@ -2,9 +2,18 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from channels.db import database_sync_to_async
 from Message.models import Conversation, Messages
+from Account.models import User
 from Message.middleware import get_user_from_token
 from urllib.parse import parse_qs
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+database_sync_to_async
+def get_user_details(user):
+    user=User.objects.get(id=user)
+    return {"username": user.username, "id": user.id, "pic": user.pic.url}
 
 @database_sync_to_async
 def create_message(conversation_id, message,user):
@@ -12,20 +21,22 @@ def create_message(conversation_id, message,user):
     message = Messages.objects.create(conversation=conversation, author=user, text=message)
     message.save()
 
-# @database_sync_to_async
-# def get_conversation(conversation_id):
-#     return Conversation.objects.get(id=conversation_id)
-
 @database_sync_to_async
 def get_messages(conversation_id,limit=10,offset=0):
     conversation=Conversation.objects.get(id=conversation_id)
     messages=Messages.objects.filter(conversation=conversation).order_by("-time_stamp")[offset:limit]
     return [{"author": message.author.username, "text": message.text, "created_at": message.time_stamp.isoformat()} for message in messages]
    
-   
+@database_sync_to_async
 def get_conversations(user):
     # Fetch all conversations for the user
-    return Conversation.objects.filter(conversations=user)
+    conv=Conversation.objects.filter(participants=user)
+    conv=[{"id": conversation.id, "participants": [get_user_details(u.id) for u in conversation.participants.all() if u!=user], "updated_at": conversation.updated_at.isoformat()} for conversation in conv]
+    # for conversation in conv:
+        
+    #     # conversation.participants=[get_user_details(user) for user in conversation.participants.all() if user!=user]
+        
+    return conv
 
 class ConversationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -33,30 +44,36 @@ class ConversationConsumer(AsyncWebsocketConsumer):
         token_key = query_string.get('token', [None])[0]
         user=await get_user_from_token(token_key)
         if user is None:
+            logger.warning("Invalid token provided. Connection closed.")
             await self.close()
         self.user=user
+        
         await self.accept()
+        await self.fetch_and_send_conversations()
+
+    
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        message_type = data.get('type')
+        if message_type == 'fetch_conversations':
+            await self.fetch_and_send_conversations()
+        
         
     async def fetch_and_send_conversations(self):
         conversations = await get_conversations(self.user)
-        serialized_conversations = [
-            {
-                'id': conversation.id,
-                'title': conversation.title,
-                'created_at': conversation.created_at.isoformat(),
-                # Add more fields as needed
-            }
-            for conversation in conversations
-        ]
+        # logger.info(f"Found {len(conversations)} conversations for user {self.user.username}")
+    
 
         # Send the list of conversations to the client
         await self.send(text_data=json.dumps({
             'type': 'conversations_list',
-            'conversations': serialized_conversations
+            'conversations': conversations
         }))
         
     async def disconnect(self, code):
         return await super().disconnect(code)
+    
+    
     
 class ChatConsumer(AsyncWebsocketConsumer):
 
