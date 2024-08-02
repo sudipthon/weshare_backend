b4583372a23ef88ab963b028dff90bb9400243d2
sudipthon <sudip00kc@gmail.com>
2024-08-02 17:37:53 +0545

    changed bd to sqlite3

--- WeShare/server_settings.py
+++ WeShare/server_settings.py
@@ -5,14 +5,16 @@ import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 DEBUG = True
+DJANGO_ENV = os.environ.get("DJANGO_ENV")
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-DATABASES = {
-    "default": dj_database_url.config(
-        default=DATABASE_URL,
-        conn_max_age=600,
-    )
-}
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=DATABASE_URL,
+#         conn_max_age=600,
+#     )
+# }
+
 
 REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
@@ -26,3 +28,13 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
